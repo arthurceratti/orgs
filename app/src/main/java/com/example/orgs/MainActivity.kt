99dc577de7b0175c80431129f594eb156c891bc7
arthurceratti <arthurceratti@gmail.com>
2023-09-22 18:38:20 -0300

    comportamento toast

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : Activity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Poc Poc faz o Porcopotamo!!", Toast.LENGTH_SHORT).show()
+    }
 }
