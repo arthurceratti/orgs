f12cbc34e862026c8803a035821c6c7892133702
arthurceratti <arthurceratti@gmail.com>
2023-10-04 22:04:00 -0300

    text_view_product form

---
+++ app/src/main/java/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,11 @@
+package com.alura.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.orgs.R
+
+class FormularioProdutoActivity :
+    AppCompatActivity(R.layout.activity_formulario_produto) {
+
+
+}
--- app/src/main/java/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/alura/orgs/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.alura.orgs.ui.activity
 import android.app.Activity
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
@@ -11,11 +12,11 @@ import com.alura.orgs.model.Produto
 import java.math.BigDecimal
 
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
 //        val nome = findViewById<TextView>(R.id.nome)
 //        nome.text = "Cesta de frutas"
 //        val descricao = findViewById<TextView>(R.id.descricao)
