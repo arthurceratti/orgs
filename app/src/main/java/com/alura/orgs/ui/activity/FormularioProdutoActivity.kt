901f9ab401c678ade240461843600fe4b710fc44
arthurceratti <arthurceratti@gmail.com>
2023-10-07 22:25:05 -0300

    07102023

--- app/src/main/java/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,10 +2,21 @@ package com.alura.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.EditText
 import com.example.orgs.R
 
 class FormularioProdutoActivity :
     AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val campoNome = findViewById<EditText>(R.id.nome)
+        val nome = campoNome.text.toString()
+        Log.i("FormularioProduto","onCreate:$nome")
+
+    }
 
 
 }
